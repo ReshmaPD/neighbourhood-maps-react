8547f0e1361cfc20a6e2b01d0c4ad29a6e4b9efd
Reshma D <developerreshmad@gmail.com>
2018-11-24 01:09:13 +0530

    sandwork

--- fechSand2.js
+++ fechSand2.js
@@ -89,3 +89,236 @@ const api = axios.create({
       this.setState({ error, isLoading: false });
     }
   }
+  GET https://api.foursquare.com/v2/venues/VENUE_ID
+
+
+
+
+
+
+
+
+
+
+
+  //====================================================================================================================
+  getFourSquareData = () => {
+  const newPlaces = this.state.allocations.map(place => {
+  export const getFourSquareVenueInfo = () => {
+    return  axios.get(
+      `https://api.foursquare.com/v2/venues/${places.venue.id}?
+      client_id=MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP
+      &client_secret=IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC
+      &v=20182011`
+    )
+    .then(response => response.data.response.venue);
+  };
+  return place;
+});
+this.setState({ currentPlaces: newPlaces });
+};
+
+
+getVenuInfo = async (query, location) => {
+    const endPoint = "https://api.foursquare.com/v2/venues/search?";
+    const params = {
+      client_id: "MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP",
+      client_secret: "IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC",
+      query: query,
+      near: location,
+      v: "20182010"
+    };
+
+    // Fetch
+    // const fetchData = async () => {
+    const response = await axios.get(endPoint + new URLSearchParams(params));
+    try {
+      this.setState({
+        allPlaces: response.data.response.groups[0].items,
+        places: response.data.response.groups[0].items,
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState({ error: error, isLoading: false });
+    }
+  };
+  //===========================================================2nd request==============================================
+  getFourSquareData = () => {
+    if (!this.state.isLoading) {
+      const newPlaces = this.state.places.map(place => {
+        // const getFourSquareVenueInfo = async () => {
+        const response = axios.get(
+          `https://api.foursquare.com/v2/venues/${this.state.places.venue.Id}?
+        client_id=MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP
+        &client_secret=IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC
+        &v=20182011`
+        );
+        try {
+          this.setState({
+            allVenue: response.data.response.venue,
+            isLoading: false
+          });
+        } catch (error) {
+          this.setState({ error: error, isLoading: false });
+        }
+        return place;
+        // }
+      });
+      this.setState({ currentPlaces: newPlaces });
+    }
+  };
+//=================================================================================================================
+
+getFourSquareData = () => {
+    if (!this.state.isLoading) {
+      const endPoint = "https://api.foursquare.com/v2/venues/";
+      const params = {
+        venueId: this.state.places.venue.id,
+        client_id: "MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP",
+        client_secret: "IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC",
+        v: "20182010"
+      };
+      const newPlaces = this.state.places.map(place => {
+        // let venueId = this.state.places.venue.id;
+        // const getFourSquareVenueInfo = async () => {
+        const response = axios.get(endPoint + new URLSearchParams(params));
+        try {
+          this.setState({
+            allVenue: response.data.response.venue,
+            isLoading: false
+          });
+        } catch (error) {
+          this.setState({ error: error, isLoading: false });
+        }
+        return place;
+        // }
+      });
+      this.setState({ currentPlaces: newPlaces });
+    }
+  };
+  //======================================
+  getFourSquareData = () => {
+    if (!this.state.isLoading) {
+      const endPoint = "https://api.foursquare.com/v2/venues/";
+      const params = {
+        venueId: this.state.venueID,
+        client_id: "MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP",
+        client_secret: "IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC",
+        v: "20182010"
+      };
+      const newPlaces = this.state.places.map(place => {
+        // let venueId = this.state.places.venue.id;
+        // const getFourSquareVenueInfo = async () => {
+        const response = axios.get(endPoint + new URLSearchParams(params));
+        try {
+          this.setState({
+            allVenue: response.data.response.venue,
+            isLoading: false
+          });
+        } catch (error) {
+          this.setState({ error: error, isLoading: false });
+        }
+        return place;
+        // }
+      });
+      this.setState({ currentPlaces: newPlaces });
+    }
+  };
+  //
+  getFourSquareData = () => {
+    if (!this.state.isLoading) {
+      const newPlaces = this.state.places.map(place => {
+        // const getFourSquareVenueInfo = async () => {
+        const response = axios.get(
+          `https://api.foursquare.com/v2/venues/${this.state.venueID}?
+        client_id=MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP
+        &client_secret=IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC
+        &v=20182011`
+        );
+        try {
+          this.setState({
+            allVenue: response.data.response.venue,
+            isLoading: false
+          });
+        } catch (error) {
+          this.setState({ error: error, isLoading: false });
+        }
+        return place;
+        // }
+      });
+      this.setState({ currentPlaces: newPlaces });
+    }
+  };
+  //========================================================MARKER==============================================
+  let infowindow = new window.google.maps.InfoWindow();
+    // Create InfoWindow
+    this.state.places.map(place => {
+      // Create Markers
+      // let marker = new window.google.maps.Marker({
+      //   position: {
+      //     lat: place.venue.location.lat,
+      //     lng: place.venue.location.lng
+      //   },
+      //   map: map,
+      //   animation: window.google.maps.Animation.DROP,
+      //   title: place.venue.name
+      // });
+
+      // Add each created marker to the 'markers' array
+      // this.state.markers.push(marker);
+
+      // Create InfoWindow
+      let content = `
+                            <h1>${place.venue.name}</h1>
+                            <p>Address: ${
+                              place.venue.location.formattedAddress[0]
+                            } ${place.venue.location.formattedAddress[1]} ${
+        place.venue.location.formattedAddress[2]
+      }</p>
+                            <p>lat: ${place.venue.location.lat}, long: ${
+        place.venue.location.lng
+      }</p>
+                            `;
+
+      // Display the InfoWindow after clicking on the Marker
+      // marker.addListener("click", function() {
+      // Update 'InfoWindow' content
+      infowindow.setContent(content);
+
+      // Open An 'InfoWindow'
+      // infowindow.open(map, marker);
+
+      // Animate The Marker
+      // if (marker.getAnimation() !== null) {
+      //   marker.setAnimation(null);
+      // } else {
+      //   marker.setAnimation(window.google.maps.Animation.BOUNCE);
+      // }
+      // });
+      return place;
+    });
+    //==============================================================================================================
+    // Get venue ID
+export const getFourSquareVenueID = (lat, lng, name) => {
+  return fetch(
+    `https://api.foursquare.com/v2/venues/search?client_id=Q5MK2XFDK3FVTDQLOQKSFTKS1CI1XEWZSO2TIPP5DU2PWICK
+    &client_secret=MQ3CZLR5KY1F04FUAX5YWXOLYRRJSYFWCHZANZZ23M4WI05L
+    &v=20130815&limit=1
+    &ll=${lat},${lng}
+    &query=${name}`
+  )
+    .then(response => response.json())
+    .then(response => response.response.venues[0].id);
+};
+
+// Get venue info data using the venue's ID
+export const getFourSquareVenueInfo = venueId => {
+  return fetch(
+    `https://api.foursquare.com/v2/venues/${venueId}?
+    client_id=Q5MK2XFDK3FVTDQLOQKSFTKS1CI1XEWZSO2TIPP5DU2PWICK
+    &client_secret=MQ3CZLR5KY1F04FUAX5YWXOLYRRJSYFWCHZANZZ23M4WI05L
+    &v=20130815`
+  )
+    .then(response => response.json())
+    .then(response => response.response.venue);
+};
--- src/data/FourSquareAPI2.js
+++ src/data/FourSquareAPI2.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 //
-export const getFourSquareVenueID = async (lat, lng, name) => {
-  return await axios
+export const getFourSquareVenueID = (lat, lng, name) => {
+  return axios
     .get(
-      `https://api.foursquare.com/v2/venues/search?client_id=MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP&client_secret=IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC&v=20182011&limit=1&ll=${lat},${lng}&query=${name}`
+      `https://api.foursquare.com/v2/venues/search?client_id=X4CMVBAJQSVZYXB45ZGE3GNA43RTCMPQTM4PUIKMQHFYWUVX&client_secret=ODC00AI1UEPGLLYLVUOY1JM30NE1XADBZRJMUNXKXPSZKNTR&v=20180323&limit=1&ll=${lat},${lng}&query=${name}`
     )
     .then(response => response.data.response.venues[0].id);
 };
 //
-export const getFourSquareVenueInfo = async venueId => {
-  return await axios
+export const getFourSquareVenueInfo = venueId => {
+  return axios
     .get(
-      `https://api.foursquare.com/v2/venues/${venueId}?client_id=MDZCTQDUG0TNYDYGOUFMGCKH322MHRGPKXKX4OVGMXFFKKFP&client_secret=IE5OFGXC5N0ANX342UUEVJ0GWBO1WD1CGJFFJ3VVLN5XPAQC&v=20182011`
+      `https://api.foursquare.com/v2/venues/${venueId}?client_id=X4CMVBAJQSVZYXB45ZGE3GNA43RTCMPQTM4PUIKMQHFYWUVX&client_secret=ODC00AI1UEPGLLYLVUOY1JM30NE1XADBZRJMUNXKXPSZKNTR&v=20180323`
     )
     .then(response => response.data.response.venue);
 };
