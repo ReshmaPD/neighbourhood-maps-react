bdc35f4b024a52fafaf20958054d4852973b2475
Reshma D <developerreshmad@gmail.com>
2018-11-25 01:59:43 +0530

    changed func to arrow func n checked error handling works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import locations from "./data/locations.json";
-// import locations2 from "./data/newPlaces2.json";
 import locations2 from "./data/newerPlaces3.json";
-// import axios from "axios";
 // =========================================================COMPONENTS============================================
 import ErrorBoundary from "./components/ErrorBoundary";
 import Map from "./components/Map";
-// import InfoWindow from "./components/InfoWindow";
 import "./App.css";
 // ================================================================================================================
 const axios = require("axios");
@@ -20,42 +16,39 @@ class App extends Component {
   };
 
   static defaultProps = {
-    zoom: 11.5,
+    zoom: 14.5,
     initialCenter: {
       lat: 18.938792382629718,
       lng: 72.82594448102758
-    },
-    alllocations: locations2
+    }
+    // alllocations: locations2
   };
   // =======================================================CONSTRUCTOR STATE============================================
   constructor(props) {
     super(props);
     const { lat, lng } = this.props.initialCenter;
     this.state = {
-      map: "",
-      query: "",
-      loaded: false,
       currentLocation: {
         lat: lat,
         lng: lng
       },
-      alllocations: locations2,
+      map: "",
+      query: "",
+      infowindow: "",
+      error: null,
+      requestSolved: null,
+      isLoading: true,
+      loaded: false,
       places2: locations2,
-      markers: [],
-      markerProp: [],
+      alllocations: locations2,
       allPlaces: [],
       places: [],
-      isLoading: true,
-      error: null,
-      infowindow: "",
-      contents: [],
-      venue2: [],
-      requestSolved: null
+      markers: [],
+      contents: []
     };
   }
 
   componentDidMount() {
-    // this.displayMap();
     this.getPlaces("sights");
   }
 
@@ -65,7 +58,7 @@ class App extends Component {
       client_id: "QV5MGPULNHAO5GRFVSWB04I023O4HYR1EQUKGDSL23F5NVZO",
       client_secret: "4ETVXDDVKL0T0DASWLZK52052YDD04CNY0WUSHFQPBNTIWUI",
       query: query,
-      near: "mumbai",
+      near: "18.938792382629718,72.82594448102758",
       v: "20181123"
     };
 
@@ -91,6 +84,10 @@ class App extends Component {
       })
       .then(() => {
         this.getmap();
+        if (this.state.requestSolved === true) {
+          this.setState({ places2: this.state.places });
+          console.log("I have setState ", this.state.places);
+        }
         // always executed
       });
   };
@@ -103,10 +100,6 @@ class App extends Component {
 
   // =================================================MAP FUNCT=====================================================
   displayMap = () => {
-    if (this.state.requestSolved === true) {
-      this.setState({ places2: this.state.places });
-      console.log("I have setState ", this.state.places);
-    }
     const apiKey = "AIzaSyDNIiyRSDZFSZXoZz1lasmM-KOXnMIhgSQ";
     loadJS(
       `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`
@@ -130,7 +123,6 @@ class App extends Component {
     // =======================================================Markers=================================================
     const markers = [];
     const contents = [];
-    let markerProps = [];
     const infowindow = new window.google.maps.InfoWindow();
     this.state.places2
       .filter(location =>
@@ -146,12 +138,12 @@ class App extends Component {
           this.state.error === null &&
           this.state.requestSolved === true
             ? `<h1>${location.venue.name}</h1>
-              <p>Address: ${location.venue.location.formattedAddress[0]}
+              <p>Address:${location.venue.location.formattedAddress[0]}
              ${location.venue.location.formattedAddress[1]}
               ${location.venue.location.formattedAddress[2]}</p>
-              <p>lat: ${location.venue.location.lat},
-               long: ${location.venue.location.lng}</p>
-               <h2>Powered By FourSquare</h2>
+              <p>lat: ${location.venue.location.lat.toFixed(2)},
+              lng: ${location.venue.location.lng.toFixed(2)}</p>
+              <h2>Powered By FourSquare</h2>
           `
             : `
         <div class="info-content">
@@ -160,7 +152,6 @@ class App extends Component {
         </div>
         <h2>Exceeded FourSquare API limit</h2>`;
         // create a marker for each location
-        // let key;
         let animation = window.google.maps.Animation.DROP;
         let marker = new window.google.maps.Marker({
           position: new window.google.maps.LatLng(location.venue.location),
@@ -169,41 +160,34 @@ class App extends Component {
           title: location.venue.name,
           animation
         });
-        //DOG
-        let mProps = {
-          key: index,
-          index,
-          name: location.venue.name,
-          position: location.venue.location
-        };
-        markerProps.push(mProps);
         location.marker = marker;
         location.display = true;
         markers.push(marker);
         contents.push(contentString);
         // set the info window content to location info and open on marker click
-        marker.addListener("click", function() {
+        marker.addListener("click", () => {
           infowindow.setContent(contentString);
-          // this.infowindow.open(map,marker);
           infowindow.open(map, marker);
           // animate the markers on click
           marker.setAnimation(window.google.maps.Animation.BOUNCE);
-          setTimeout(function() {
+          setTimeout(() => {
             marker.setAnimation(null);
           }, 850);
         });
         // close info windows when map is clicked
-        map.addListener("click", function() {
+        map.addListener("click", () => {
           if (infowindow) {
             infowindow.close();
+          }
+          else{
+            this.state.map.setCenter(marker.getPosition());
+            this.state.map.panBy(0, -200);
           }
         });
       });
     this.setState({ map, markers, infowindow, contents, loaded: true });
-    this.setState({ map, loaded: true });
   }
   //===================================================================================================================
-  getMarkerInfo = (marker, map) => {};
 
   //=======================================================RENDER======================================================
   render() {
@@ -228,8 +212,8 @@ function loadJS(url) {
   script.async = true;
   script.defer = true;
   ref.parentNode.insertBefore(script, ref);
-  script.onerror = function() {
-    document.write("Error loading Map, Try again in a few moments.");
+  script.onerror = () => {
+    document.write("Error loading Map, Please Try again in a few moments.");
   };
 }
 //=====================================================================================================================
